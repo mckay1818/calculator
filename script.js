9e147371e25287dafa5da350668b3d89fc05420e
mckay1818 <72710253+mckay1818@users.noreply.github.com>
2021-08-24 21:44:57 -0400

    Initial commit with HTML calculator skeleton

---
+++ script.js
@@ -0,0 +1,38 @@
+let resultDisplay = document.getElementById("result-display");
+
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return add(a, b);
+            break;
+        default:
+            return null;
+            //break unnecessary after last case
+    }
+}
